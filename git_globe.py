edf5b0b666c419cbe633ef4ba76da025ce594944
Eoin Morgan <eoinmorgan@berkeley.edu>
2014-12-03 11:35:22 -0800

    removed my tokens/auth details

--- git_globe.py
+++ git_globe.py
@@ -57,8 +57,3 @@ def get_location_coords(repo_owner, repo_name, git_globe_config):
 	return [(login, get_coords(get_location_name(login, git_globe_config), git_globe_config))
     	for login in get_repo_contributors(repo_owner, repo_name, git_globe_config)]
 
-git_globe_config = {}
-git_globe_config["oauth_token"] = "eb9f1c1e55be9fdd7daecdd16540b21edcbafce0"
-git_globe_config["mapquest_api_key"] = "Fmjtd%7Cluurn16a2d%2Caw%3Do5-9wts1z"
-
-pprint(get_location_coords("defunkt", "dotjs", git_globe_config))
